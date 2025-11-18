049af5bf62453b83ff0e985bc86dbf5ecc569d8a
Osilvfe <64585305+Osilvfe@users.noreply.github.com>
2025-11-19 01:59:50 +0800

    Modify meson setup to include --libdir option
    Added --libdir option to meson setup for correct installation path.

--- container/build-turnip-driver.sh
+++ container/build-turnip-driver.sh
@@ -17,7 +17,8 @@ build() {
     apt-get -y install python3-mako python3-yaml zlib1g-dev libexpat1-dev libdrm-dev bison flex cmake wayland-protocols \
     libwayland-dev xorg-dev libxml2-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-glx0-dev libxcb-present-dev libxcb-randr0-dev \
     libxcb-shm0-dev libxcb-sync-dev libxcb-xfixes0-dev libxshmfence-dev libxcb1-dev libx11-xcb-dev libxcb-keysyms1-dev
-    meson setup '-Dfreedreno-kmds=['\''msm'\'','\''kgsl'\'']' '-Dgallium-drivers=[]' '-Dvulkan-drivers=['\''freedreno'\'']'
+    # Added --libdir to ensure correct install path for copy_built_files.sh
+    meson setup '-Dfreedreno-kmds=['\''msm'\'','\''kgsl'\'']' '-Dgallium-drivers=[]' '-Dvulkan-drivers=['\''freedreno'\'']' --libdir=lib/aarch64-linux-gnu
     ninja
 }
 
