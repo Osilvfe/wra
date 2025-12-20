9c2b554b01a3e485216bbe27eb5b06e0975719d3
Osilvfe <3072756864@qq.com>
2025-12-21 00:40:55 +0800

    Change debian version to trixie to avoid lib problem

--- scripts/prepare_build_env.sh
+++ scripts/prepare_build_env.sh
@@ -9,14 +9,14 @@ mkdir -p $BUILD_ROOT
 cd "$BUILD_ROOT" || exit 1
 
 if [ ! -d "rootfs-build" ]; then
-    _notice_ "Bootstraping Debian sid...in "$(pwd)"/rootfs-build for build"
-    debootstrap --arch=arm64 sid rootfs-build
+    _notice_ "Bootstraping Debian trixie...in "$(pwd)"/rootfs-build for build"
+    debootstrap --arch=arm64 trixie rootfs-build
 else
     echo $(pwd)"/rootfs-build already exists,skiping"
 fi
 
 if [ ! -d "rootfs" ]; then
-    _notice_ "Copy Debian sid...to "$(pwd)"/rootfs for run"
+    _notice_ "Copy Debian trixie...to "$(pwd)"/rootfs for run"
     cp -r rootfs-build rootfs
 else
     echo $(pwd)"/rootfs already exists,skiping"
